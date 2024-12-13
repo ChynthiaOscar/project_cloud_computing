bc2cfb71f7965e04dd63e0023add84466c1ee1d2
FamousTeen <125239075+FamousTeen@users.noreply.github.com>
2024-12-13 11:25:15 +0700

    ubah DrawController

--- app/Http/Controllers/DrawController.php
+++ app/Http/Controllers/DrawController.php
@@ -2,27 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Draw;
 use App\Models\Draws;
-use App\Models\Tickets_Detail;
 use Illuminate\Http\Request;
 
 class DrawController extends Controller
 {
     public function getDrawData()
     {
-        $quota = Tickets_Detail::sum('quota');
-        if ($quota == 0) {
-            return response()->json(['message' => 'No tickets available']);
-        } else {
-            $draws = Draws::select('id', 'account_id', 'package_id')
+        $draws = Draws::select('id', 'account_id', 'package_id')
             ->where('status', 0)
             ->orderBy('package_id')
             ->get()
             ->groupBy('package_id'); // Group by package_id
 
-            return response()->json($draws);    
-        }
-        
+        return response()->json($draws);
     }
 
     public function saveWinners(Request $request)
